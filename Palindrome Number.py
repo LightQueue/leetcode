e89d2025968b12d57022c53c00cda386a8024098
Qinglong Lee <leeqinglong@qq.com>
2018-03-10 10:29:00 +0800

    Palidrome number python edition

---
+++ Palindrome Number.py
@@ -0,0 +1,18 @@
+#2nd edition 272ms
+class Solution:
+    def isPalindrome(self, x):
+        return True if x == int(str(abs(x))[::-1]) else False
+
+#1st edition 272ms
+class Solution:
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        else :
+            return True if x == int(str(x)[::-1]) else False
+
+'''
+第二版受到discuss上的启发，把判断语句消去了。
+当x是负数时，|x|!=x
+'''
+
