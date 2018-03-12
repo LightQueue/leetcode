bcfdc7e66439308a1879419689298c1e3fe4d659
Qinglong Lee <leeqinglong@qq.com>
2018-03-12 15:59:04 +0800

    longest common prefix pyton edition

--- 新建文本文档.py
+++ 新建文本文档.py
@@ -1,14 +1,13 @@
-#1st edition 48ms
 class Solution:
     def longestCommonPrefix(self, strs):
         if not strs:
             return ''
         result = ''
+        if len(strs) == 1:
+            return strs[0]
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
                 if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
                     return result
             result += strs[0][i]
         return result
-        
-
